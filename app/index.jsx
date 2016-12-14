528bb531a182e83ca88c8c91454fb714d9db8281
dharam@epicenterlabs.com <dharam@epicenterlabs.com>
2016-12-15 02:27:17 +0530

    action,dispatcher,store working

--- app/index.jsx
+++ app/index.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import constants from './constants/app-constants.js'
 import {incrementActions} from './actions/app-actions.js'
+import {TodoStore} from './stores/count-store.js'
 console.log(constants)
 
 
@@ -10,13 +11,27 @@ class App extends React.Component {
 
 	constructor(props) {
 		super(props)
-		this.state = {count: 0}
+		this.state = {count: TodoStore.getCount()}
 		this.increment = this.increment.bind(this)
+		this._onChange = this._onChange.bind(this)
 	}
 	
 	increment() {
 		incrementActions.incrementCount()
 	}
+
+	_onChange() {
+		this.setState({count: TodoStore.getCount()})
+	}
+
+	componentDidMount() {
+		TodoStore.addChangeListener(this._onChange)
+	}
+
+	componentWillUnmount() {
+		TodoStore.removeListener(this._onChange)
+	}
+
 
 	render() {
 		return (
